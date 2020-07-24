2fe89bb75eb716378cff5ab41e06ca80a54ce0f2
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-07-24 05:41:42 +0200

    Deprecate compat.bytesEnviron() and remove its uses
    There were two callers inside Twisted: test_compat and test_process.
    
    The code in test_compat is the unit test and it checks that the
    returned environment dictionary indeed contains only keys and values
    of type 'bytes'. I left this as-is.
    
    The code in test_process was weird: this is a Windows-specific test,
    while bytesEnviron() claims to be POSIX-only in its docstring. It
    turns out that bytesEnviron() doesn't break when executed on
    Windows, but it produces an environment with keys and values as
    strings instead of bytes.
    
    The test passes despite this mismatch, but to reduce the confusion
    I removed the call and replaced it with a simple copy of os.environ,
    so it is more clear what is actually being tested here: an environment
    with mostly string entries and one entry in bytes.

--- src/twisted/test/test_process.py
+++ src/twisted/test/test_process.py
@@ -53,7 +53,7 @@ from twisted.python.log import msg
 from twisted.internet import reactor, protocol, error, interfaces, defer
 from twisted.trial import unittest
 from twisted.python import runtime, procutils
-from twisted.python.compat import networkString, bytesEnviron
+from twisted.python.compat import networkString
 from twisted.python.filepath import FilePath
 
 
@@ -2263,7 +2263,7 @@ class Win32ProcessTests(unittest.TestCase):
 
         pyExe = FilePath(sys.executable)._asBytesPath()
         args = [pyExe, b"-u", b"-m", b"twisted.test.process_stdinreader"]
-        env = bytesEnviron()
+        env = dict(os.environ)
         env[b"PYTHONPATH"] = os.pathsep.join(sys.path).encode(
                                              sys.getfilesystemencoding())
         path = win32api.GetTempPath()
